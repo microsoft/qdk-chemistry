5634b88bf8e24557a2bbbf8eec77e96103b76389
Marco Eckhoff <v-meckhoff@microsoft.com>
2025-11-21 18:16:55 +0100

    Improve formatting

--- python/tests/test_structure.py
+++ python/tests/test_structure.py
@@ -1,3 +1,5 @@
+"""Tests for Structure class and related functionality in QDK/Chemistry."""
+
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
