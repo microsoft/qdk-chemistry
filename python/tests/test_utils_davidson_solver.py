f4d1c4485d5c2445b1ace34878c1678a37a1049e
Rushi Gong <v-rushigong@microsoft.com>
2025-11-26 18:35:52 +0000

    add docstrings for test files

--- python/tests/test_utils_davidson_solver.py
+++ python/tests/test_utils_davidson_solver.py
@@ -1,3 +1,5 @@
+"""Test for Davidson solver utility."""
+
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
--- python/tests/test_utils_syev_solver.py
+++ python/tests/test_utils_syev_solver.py
@@ -1,3 +1,5 @@
+"""Test for LAPACK SYEV solver utility."""
+
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
