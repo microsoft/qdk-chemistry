89a579bcefafa8be74bd345692d9626bf7bc6d05
alexismills <alexismills@microsoft.com>
2025-12-02 17:58:40 +0000

    fix hook failures

--- setup_dev_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# filepath: /anfhome/alexismills/qatk/setup_dev_env.sh
-
-export QDK_CHEMISTRY_RESOURCES_PATH="/anfhome/alexismills/qatk/cpp/src/qdk/chemistry/algorithms/microsoft/scf/resources"
-export PYTHONPATH="/anfhome/alexismills/qatk/python/src:$PYTHONPATH"
