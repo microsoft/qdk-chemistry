da8dfd3de9b1bbaad9415b7db181f23edee0281e
Nathan Baker <nathanbaker@microsoft.com>
2025-12-03 11:57:12 -0800

    Make in-text citations less verbose.

--- python/src/qdk_chemistry/phase_estimation/iterative_qpe.py
+++ python/src/qdk_chemistry/phase_estimation/iterative_qpe.py
@@ -5,8 +5,7 @@ algorithm, which measures phase bits sequentially from most-significant to least
 using a single ancilla qubit and adaptive feedback corrections.
 
 References:
-    Kitaev, A. (1995). "Quantum measurements and the Abelian Stabilizer Problem."
-    arXiv:quant-ph/9511026. :cite:`Kitaev1995`
+    Kitaev, A. (1995). arXiv:quant-ph/9511026. :cite:`Kitaev1995`
 
 """
 
