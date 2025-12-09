80a098aad4a38a20736eb0fa75ce7993d6e6986e
Max Nishibuchi <v-georgen@microsoft.com>
2025-12-09 08:15:25 -0800

    Arch specific explicit int size spec

--- .pipelines/install-scripts/install-blis.sh
+++ .pipelines/install-scripts/install-blis.sh
@@ -4,14 +4,6 @@ MARCH=${2:-x86-64-v3}
 BLIS_VERSION=${3:-2.0}
 CFLAGS=${4:-"-fPIC -O3"}
 
-# Select architectures to build BLIS for
-if [[ ${MARCH} == 'armv8-a' ]]; then
-    # Compile for armsve, firestorm, thunderx2, cortexa57, cortexa53, and generic architectures
-    export BLIS_ARCH=arm64
-elif [[ ${MARCH} == 'x86-64-v3' ]]; then
-    # Compile for intel64, amd64, and amd64_legacy architectures
-    export BLIS_ARCH=x86_64
-fi
 # Download BLIS v2.0
 echo "Downloading BLIS ${BLIS_VERSION}..."
 export BLIS_CHECKSUM=1cccc40e6d4dff71911856e4f5782405fe76fc93
@@ -23,11 +15,26 @@ mv blis-${BLIS_VERSION} blis
 
 cd blis
 
-CFLAGS=${CFLAGS} ./configure \
+# Select architectures to build BLIS for
+if [[ ${MARCH} == 'armv8-a' ]]; then
+    # Compile for armsve, firestorm, thunderx2, cortexa57, cortexa53, and generic architectures
+    export BLIS_ARCH=arm64
+    CFLAGS=${CFLAGS} ./configure \
+    --disable-shared \
+    --enable-static \
+    --prefix=${INSTALL_PREFIX} \
+    $BLIS_ARCH \
+    --blis-int-size=32 \
+    --int-size=32
+elif [[ ${MARCH} == 'x86-64-v3' ]]; then
+    # Compile for intel64, amd64, and amd64_legacy architectures
+    export BLIS_ARCH=x86_64
+    CFLAGS=${CFLAGS} ./configure \
     --disable-shared \
     --enable-static \
     --prefix=${INSTALL_PREFIX} \
     $BLIS_ARCH
+fi
 
 make -j$(nproc)
 make install
