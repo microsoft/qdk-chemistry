230e3f69cfe228613e89adf7ccb6a198c8220446
MaxNishibuchi <v-georgen@microsoft.com>
2025-12-18 14:33:56 -0800

    Limit Parallelism in Dev Container Build (#228)

--- .devcontainer/scripts/venv_and_widget.sh
+++ .devcontainer/scripts/venv_and_widget.sh
@@ -17,11 +17,11 @@ source $HOME/.cargo/env
 which rustc
 
 # Install ipykernel
-pip install ipykernel ipywidgets pandas rdkit
+pip install ipykernel ipywidgets pandas pre-commit rdkit
 
 # Install QDK Chemistry package
-cd "$REPO_ROOT/python"
-export CMAKE_BUILD_PARALLEL_LEVEL=4
+cd ./python
+export CMAKE_BUILD_PARALLEL_LEVEL=2
 QDK_UARCH=native pip install -v .[all]
 
 # Install QDK Widgets
