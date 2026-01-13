b6f9de7cd579010285764b7f58e77199f806861c
Max Nishibuchi <v-georgen@microsoft.com>
2026-01-13 10:54:39 -0800

    Add dummy package for initial PyPI publishing

---
+++ temp-release/src/qdk_chemistry/__init__.py
@@ -0,0 +1,9 @@
+"""Module description."""
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+def main():
+    print("qdk-chemistry, coming soon!")
