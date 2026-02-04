7fd598b6cf8187e627c5c73d2f0ae4c647ca3130
rainli323 <rainli323@gmail.com>
2026-02-04 22:00:45 +0000

    docs warning

--- cpp/include/qdk/chemistry/algorithms/hamiltonian.hpp
+++ cpp/include/qdk/chemistry/algorithms/hamiltonian.hpp
@@ -113,6 +113,10 @@ class HamiltonianConstructor
    * called by run() after settings have been locked.
    *
    * @param orbitals The orbital data from which to construct the Hamiltonian
+   * @param aux_basis Optional auxiliary basis set for density-fitted
+   *        implementations. Can be a BasisSet pointer or a string name.
+   *        Implementations that don't use density fitting should not provide
+   * this.
    * @return The constructed Hamiltonian operator
    */
   virtual std::shared_ptr<data::Hamiltonian> _run_impl(
