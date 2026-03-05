eb9c7f2b5a8dcf90ae36b34263d84d2acfbe32e8
v-boqinzhang <v-boqinzhang@microsoft.com>
2026-03-04 16:37:02 -0800

    add while loop

--- python/src/qdk_chemistry/algorithms/phase_estimation/dynamic_mode_decomposition.py
+++ python/src/qdk_chemistry/algorithms/phase_estimation/dynamic_mode_decomposition.py
@@ -52,8 +52,9 @@ class DynamicModeDecompositionSettings(PhaseEstimationSettings):
             "The number of shots to execute per observable (overlap \bra{\psi}\exp{-i\hat{H} n dt}\ket{\psi})).",
         )
         self._set_default("vector_dim", "int", -1, "the row number of Hankel matrix X")
-        self._set_default("rank_k", "int", -1, "the column number of Hankel matrix X")
+        self._set_default("initial_rank_k", "int", -1, "the initial column number of Hankel matrix X")
         self._set_default("shots_per_observable", "int", 20, "the number of shots to test one observable")
+        self._set_default("max_rank_k", "int", 100, "Column number limit of Hankel matrix X")
 
 class DynamicModeDecomposition(PhaseEstimation):
     """ODMD algorithm implementation."""
@@ -61,22 +62,26 @@ class DynamicModeDecomposition(PhaseEstimation):
     def __init__(
         self,
         vector_dim: int,
-        rank_k: int,
+        initial_rank_k: int,
         evolution_time: float,
-        shots_per_observable: int = 20
+        shots_per_observable: int = 20,
+        max_rank_k: int = 100
     ):
         Logger.trace_entering()
         super().__init__(evolution_time=evolution_time)
         self._settings = DynamicModeDecompositionSettings()
         self._settings.set("vector_dim", vector_dim)
-        self._settings.set("rank_k", rank_k)
+        self._settings.set("initial_rank_k", initial_rank_k)
         self._settings.set("evolution_time", evolution_time)
         self._settings.set("shots_per_observable", shots_per_observable)
+        self._settings.set("max_rank_k", max_rank_k)
         self._iteration_circuits: list[Circuit] | None = None
 
         # check validity of inputs
-        if (vector_dim < 1 or rank_k < 1):
-            raise ValueError("vector_dim and rank_k must be a positive integer.")
+        if (vector_dim < 1 or initial_rank_k < 1):
+            raise ValueError("vector_dim and initial_rank_k must be a positive integer.")
+        if (initial_rank_k > max_rank_k):
+            raise ValueError("initial_rank_k must be no more than max_rank_k.")
         if (evolution_time < 0.0):
             raise ValueError("evolution_time must be a positive double number.")
         if (shots_per_observable < 1): # currently we use classical Hadamard test for measuring observable
@@ -105,19 +110,22 @@ class DynamicModeDecomposition(PhaseEstimation):
             QpeResult: The result of the phase estimation.
 
         """
-        # quantum part: obtain the observables (overlaps) by Hadamard tests
+        
         vector_dim = self._settings.get("vector_dim")
-        rank_k = self._settings.get("rank_k")
-        total_number_measurement = vector_dim + rank_k
-        observable_array = np.zeros(total_number_measurement)
+        initial_rank_k = self._settings.get("initial_rank_k")
+        max_rank_k = self._settings.get("max_rank_k")
+        initial_number_measurement = vector_dim + initial_rank_k
+        observable_array = np.zeros(vector_dim + max_rank_k)
         shots = self._settings.get("shots_per_observable")
 
+        # generate controlled U = exp(-iH dt) by evolution_builder, it will be connected by circuit_mapper
         ctrl_time_evol_unitary = self.create_ctrl_one_time_evol(
             qubit_hamiltonian, 
             evolution_builder
         )
 
-        for i in range (0, total_number_measurement):
+        # calculate initial vector_dim + initial_rank_k - 1 observables
+        for i in range (0, initial_number_measurement - 1):
             circuit_real = self.create_iteration_circuit(
                 state_preparation,
                 qubit_hamiltonian.num_qubits,
@@ -133,34 +141,70 @@ class DynamicModeDecomposition(PhaseEstimation):
             Logger.info(f"measured real part of {i + 1} observable, value {observable_real}")
             observable_array[i] = observable_real
 
-        # classical part: generate Hankel matrices, pseudo-inverse to get DMD matrix A
+        last_phase_fraction = 1.0
+        phase_fraction = 0.0
+        observe_index = initial_number_measurement - 2
+        rank_k = initial_rank_k - 1
+
+        # generate Hankel matrices
         X = np.zeros((vector_dim, rank_k))
         for v in range(0, rank_k):
             X[:, v] = observable_array[v : v + vector_dim]
         X_prime = np.zeros((vector_dim, rank_k))
         X_prime[:, 0:rank_k - 1] = X[:, 1:rank_k]
         X_prime[:, rank_k - 1] = observable_array[rank_k : rank_k + vector_dim]
-        A = X_prime @ np.linalg.pinv(X)
 
-        # classical part: extract ground state energy from A by diagonalization
-        eigenvalues = np.linalg.eigvals(A) # eigensolver for general matrix
+        while abs(phase_fraction - last_phase_fraction) > 1e-6:
+            if rank_k == max_rank_k + 1:
+                Logger.warning(f"reached the limit of rank_k {max_rank_k}! ODMD iteration will stop here!")
+                break
 
-        log_Eigs = np.log(eigenvalues)
+            last_phase_fraction = phase_fraction
 
-        print(log_Eigs)
+            # quantum part: obtain the observables (overlaps) by Hadamard tests
+            rank_k += 1
+            observe_index += 1
+            circuit_real = self.create_iteration_circuit(
+                state_preparation,
+                qubit_hamiltonian.num_qubits,
+                ctrl_time_evol_unitary,
+                circuit_mapper,
+                observe_index + 1
+            )
 
-        # Pick the mode with smallest |real| and negative imaginary part.
-        negative_imag_mask = log_Eigs.imag < 0.0
-        if np.any(negative_imag_mask):
-            candidate_log_Eigs = log_Eigs[negative_imag_mask]
-        else:
-            candidate_log_Eigs = log_Eigs
+            executor_data = circuit_executor.run(circuit_real, shots=shots, noise=noise)
+            bitstring_result = executor_data.bitstring_counts # a dictionary
+            Logger.info(f"measured real part of {observe_index + 1} observable, {bitstring_result.get("0", 0)} zeros, {bitstring_result.get("1", 0)} ones")
+            observable_real = (bitstring_result.get("0", 0) - bitstring_result.get("1", 0)) / shots
+            Logger.info(f"measured real part of {observe_index + 1} observable, value {observable_real}")
+            observable_array[observe_index] = observable_real
 
-        min_real_idx = np.argmin(np.abs(candidate_log_Eigs.real))
-        phase = candidate_log_Eigs[min_real_idx].imag
-        phase_fraction = phase / (2.0 * np.pi)
+            # classical part: update Hankel matrices, pseudo-inverse to get DMD matrix A
+            X = np.column_stack((X, X_prime[:, rank_k - 2]))
+            X_prime = np.column_stack((X_prime, observable_array[rank_k : rank_k + vector_dim]))
 
-        print(phase_fraction)
+            A = X_prime @ np.linalg.pinv(X)
+
+            # classical part: extract ground state energy from A by diagonalization
+            eigenvalues = np.linalg.eigvals(A) # eigensolver for general matrix
+
+            log_Eigs = np.log(eigenvalues)
+
+            print(log_Eigs)
+
+            # Pick the mode with smallest |real| and negative imaginary part.
+            negative_imag_mask = log_Eigs.imag < 0.0
+            if np.any(negative_imag_mask):
+                candidate_log_Eigs = log_Eigs[negative_imag_mask]
+            else:
+                candidate_log_Eigs = log_Eigs
+
+            min_real_idx = np.argmin(np.abs(candidate_log_Eigs.real))
+            phase = candidate_log_Eigs[min_real_idx].imag
+            phase_fraction = phase / (2.0 * np.pi)
+
+            Logger.info(f"iteration {rank_k - initial_rank_k + 1}, phase_fraction {phase_fraction}")
+
         # Create and return the result
         return QpeResult.from_phase_fraction(
             method=self.name(),
