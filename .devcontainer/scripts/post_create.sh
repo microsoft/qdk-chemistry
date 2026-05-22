716761694e6242d1a4ae6c879d2a4fa561493515
Kristjan Eimre <kristjaneimre@gmail.com>
2026-05-22 21:30:17 +0200

    dev-container: install c++ and python separately

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 source "$HOME/qdk_chemistry_venv/bin/activate"
 export CMAKE_BUILD_PARALLEL_LEVEL=4
 
+# Build C++ and install to /usr/local
+cmake -S cpp -B cpp/build -G Ninja
+cmake --build cpp/build
+sudo cmake --install cpp/build
+
 # Install python library
 cd ./python
 pip install -v .[all]
