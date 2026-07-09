668a601391020360aeeefe56daf6ebefb8f77dc9
Agam Shayit <v-agamshayit@microsoft.com>
2026-07-09 23:32:57 +0000

    started circuit_executor

--- python/src/qdk_chemistry/algorithms/registry.py
+++ python/src/qdk_chemistry/algorithms/registry.py
@@ -766,6 +766,9 @@ def _register_python_algorithms():
     This function is automatically called during module import and should not
     be called by users.
     """
+    from qdk_chemistry.algorithms.circuit_executor.azure_quantum_emulator import (  # noqa: PLC0415
+        AzureQuantumEmulator,
+    )
     from qdk_chemistry.algorithms.circuit_executor.qdk import (  # noqa: PLC0415
         QdkFullStateSimulator,
         QdkSparseStateSimulator,
@@ -840,6 +843,7 @@ def _register_python_algorithms():
     register(lambda: MagnusPropagator())
     register(lambda: QdkFullStateSimulator())
     register(lambda: QdkSparseStateSimulator())
+    register(lambda: AzureQuantumEmulator())
     register(lambda: QdkIterativeQpeCircuitBuilder())
     register(lambda: QdkStandardQpeCircuitBuilder())
     register(lambda: IterativePhaseEstimation())
