cc26dd145a61be0998dc0a53c3bfb0361152170f
Conrad Johnston <cojohnston@microsoft.com>
2026-08-10 10:48:47 -0700

    Use gauXC cube screening default
    Avoid the unnecessarily strict 1e-12 shell tolerance so cube evaluation benefits from gauXC's performance-oriented default while retaining conservative accuracy.

--- cpp/src/qdk/chemistry/cube/cube_generator.cpp
+++ cpp/src/qdk/chemistry/cube/cube_generator.cpp
@@ -141,7 +141,7 @@ struct CubeGenerator::Impl {
         gauxc_basis(to_gauxc_basis(require_basis_set(basis_set))),
         gauxc_mol(to_gauxc_mol(*require_basis_set(basis_set).get_structure())),
         evaluator(GauXC::OrbitalEvaluatorFactory::make_orbital_evaluator(
-            GauXC::ExecutionSpace::Host, gauxc_basis, 1e-12)),
+            GauXC::ExecutionSpace::Host, gauxc_basis)),
         nbf(gauxc_basis.nbf()) {}
 };
 
