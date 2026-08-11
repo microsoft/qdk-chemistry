b811f7d0301bbf8d153151f4e06190dcee9db2bf
Conrad Johnston <cojohnston@microsoft.com>
2026-08-11 14:49:33 -0700

    Fix clang-cl Gau2Grid patch detection

--- cpp/cmake/patches/gauxc-clang-cl-gau2grid-stdlib.cmake
+++ cpp/cmake/patches/gauxc-clang-cl-gau2grid-stdlib.cmake
@@ -9,14 +9,24 @@ endif()
 
 file(READ "${_helper}" _content)
 
-if(NOT _content MATCHES "#[ \t]*include[ \t]*<stdlib\\.h>")
-  if(NOT _content MATCHES "#[ \t]*include[ \t]*<math\\.h>")
+string(FIND "${_content}" "#include <stdlib.h>\n#include <math.h>" _patched_lf)
+string(FIND "${_content}" "#include <stdlib.h>\r\n#include <math.h>"
+       _patched_crlf)
+
+if(_patched_lf EQUAL -1 AND _patched_crlf EQUAL -1)
+  string(FIND "${_content}" "#include <math.h>" _math_include)
+  if(_math_include EQUAL -1)
     message(FATAL_ERROR
             "Cannot patch Gau2Grid for clang-cl: math.h include not found")
   endif()
-  string(REGEX REPLACE "(#[ \t]*include[ \t]*<math\\.h>)"
-                       "#include <stdlib.h>\n\\1"
-                       _content "${_content}")
+  if(_content MATCHES "\r\n")
+    set(_newline "\r\n")
+  else()
+    set(_newline "\n")
+  endif()
+  string(REPLACE "#include <math.h>"
+                 "#include <stdlib.h>${_newline}#include <math.h>"
+                 _content "${_content}")
   file(WRITE "${_helper}" "${_content}")
   message(STATUS "Patched Gau2Grid helper for clang-cl stdlib declarations")
 endif()
